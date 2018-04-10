7f1bfe8bf5d690a78ed4d20fc066e130a728852b
poiufd <s.volk95@mail.ru>
2018-04-10 11:53:20 +0300

    bug fixed

--- lab_3/lab3.py
+++ lab_3/lab3.py
@@ -1,7 +1,6 @@
 N = ['E','T','F']
 P = [('E','E+T'),('E','T'),('T','T*F'),('T','F'),('F','a'),('F','(E)')]
 S = 'E'
-w = 'a*a'
 
 def find_fold(imin,st):
 	suff, temp_suff, new_suff = '','',''
@@ -23,13 +22,12 @@ def find_state(res):
 		if rule[0] == 'q' and rule[2] == suff:
 			num = i + 2
 			break
-	return res[-num]		
+	if num == -1:
+		return -1
+	else:			
+		return res[-num]		
 
-def main():
-	result = []
-	st = ('q',1,'$','e')
-	result.append(st)
-	
+def algo(result,w):
 	while True:
 		#step1 - fold
 		st = result[-1]
@@ -69,32 +67,54 @@ def main():
 					#step5 - return
 					while True:
 						st = result[-1]
-						new_st = find_state(result)
-						new_st = list(new_st)
+						try:
+							new_st = find_state(result)
+							new_st = list(new_st)
 						#just rollback
-						if (st[1] == len(w) + 1) or (st[3][0] == 's'):
-							new_st[0] = 'b'	
-							result.append(tuple(new_st))			
-						else:
-							rule = int(st[3][0]) - 1
-							temp_suff, new_suff, temp_i = find_fold(rule,new_st)
-							# if found another fold
-							if temp_i != -1:
-								new_st[0] = 'q'				
-								new_st[2] = new_st[2].replace(temp_suff,new_suff)
-								new_st[3] = str(temp_i+1) + new_st[3]
-								result.append(tuple(new_st))
-								break 
+							if (st[3][0] == 's'):
+								st = list(st)
+								i = st[1]
+								st[1] = i-1
+								st[2] = st[2][:-1]		
+								st[3] = st[3][1:]	
+								result.append(tuple(st))
+							elif st[1] == len(w) + 1:
+								new_st[0] = 'b'	
+								result.append(tuple(new_st))	
 							else:
-								new_st[0] = 'q'
-								i = new_st[1]
-								new_st[1] = i+1
-								new_st[2] += w[i-1]
-								new_st[3] = 's' + new_st[3]
-								result.append(tuple(new_st))
-								break
+								rule = int(st[3][0]) - 1
+								temp_suff, new_suff, temp_i = find_fold(rule,new_st)
+							# if found another fold
+								if temp_i != -1:
+									new_st[0] = 'q'				
+									new_st[2] = new_st[2].replace(temp_suff,new_suff)
+									new_st[3] = str(temp_i+1) + new_st[3]
+									result.append(tuple(new_st))
+									break 
+								else:
+									new_st[0] = 'q'
+									i = new_st[1]
+									new_st[1] = i+1
+									new_st[2] += w[i-1]
+									new_st[3] = 's' + new_st[3]
+									result.append(tuple(new_st))
+									break
+						except:
+							raise Exception("Error")
 	
-	for state in result: print (state)
+
+def main():
+	result = []
+	st = ('q',1,'$','e')
+	w = 'a*a'
+	result.append(st)
+	
+	try:
+		algo(result,w)
+		for state in result: print (state)
+	except Exception as ex:
+		print (ex)		
+
 
 
 
